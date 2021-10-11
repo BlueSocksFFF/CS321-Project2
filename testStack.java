3fdc4d675133ec5cbbd85d5c2d338e466af41aab
student <ksdixo23@colby.edu>
2021-10-10 20:20:22 -0400

    testing constructor and push method

---
+++ testStack.java
@@ -0,0 +1,16 @@
+/**
+ * Authors: Diane Zhang, Kayla Dixon
+ * Project: CS321 Project 2 Assembler
+ * Instructor: Prof. Al Madi
+ */
+ 
+
+ public class testStack{
+
+    public static void main (String [] args){
+
+        Stack s1 = new Stack();
+        s1.push("Hello");
+        System.out.println(s1);
+    }
+ }
