86b6b3a6155b64d418feb047be9a33dea1b09d49
Diane Zhang <yizhan22@colby.edu>
2021-10-12 21:40:12 -0400

    tested Converter

--- Converter.java
+++ Converter.java
@@ -1,3 +1,5 @@
+import java.util.Stack;
+
 /**
  * Authors: Diane Zhang, Kayla Dixon
  * Project: CS321 Project 2 Assembler
@@ -8,32 +10,40 @@ public class Converter{
 
     public static void main(String[] args) {
         
-        Stack<String> s1 = new Stack<String>();
+        //Stack<String> s1 = new Stack<String>();
         if (args.length<1){
             System.out.println("Please add inputs");
-            exit();
+            System.exit(0);
         }
-        else{
-            while (i<args.length){
-                String token = args[i];
-                while (!token.equals(";")){
-                    if (token.equals(")")){
-                        String right = s1.pop();
-                        String oper = s1.pop();
-                        String left = s1.pop();
-                        s1.push(left+right+oper);
-                    }
-                    else{
-                        if (!token.equals("(")){
-                            s1.push(token);
-                        }
+        for (int i = 0; i < args.length; i++) {
+            System.out.println(args[0]);
+        }
+        /*
+        int i = 0;
+        String token = args[i];
+        while (i < args.length-1){
+            System.out.println(i);
+            System.out.println(token);
+            while (!token.equals(";")){
+                //System.out.println("looped");
+                if (token.equals(")")){
+                    String right = s1.pop();
+                    String oper = s1.pop();
+                    String left = s1.pop();
+                    s1.push(left+right+oper);
+                }
+                else{
+                    if (!token.equals("(")){
+                        s1.push(token);
                     }
                 }
                 i++;
+                token = args[i];
             }
-            System.out.println(s1.peek());
         }
+        String result = s1.peek();
+        System.out.println(result);
+        */
     }
-
 }
  
