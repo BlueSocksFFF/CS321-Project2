92d31a896c902c80a8625b7c0300d390eb1c00a3
student <ksdixo23@colby.edu>
2021-10-12 22:08:09 -0400

    Fixes #1

--- Converter.java
+++ Converter.java
@@ -1,15 +1,59 @@
-import java.util.Stack;
-
 /**
  * Authors: Diane Zhang, Kayla Dixon
  * Project: CS321 Project 2 Assembler
  * Instructor: Prof. Al Madi
  */
+import java.util.ArrayList;
 
 public class Converter{
 
     public static void main(String[] args) {
         
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("(");
+        list.add("(");
+        list.add("AX");
+        list.add("+");
+        list.add("(");
+        list.add("BY");
+        list.add("*");
+        list.add("C");
+        list.add(")");
+        list.add(")");
+        list.add("/");
+        list.add("(");
+        list.add("D4");
+        list.add("-");
+        list.add("E");
+        list.add(")");
+        list.add(")");
+        list.add(";");
+        System.out.println(list);
+
+        Stack<String> s1 = new Stack<String>();
+
+        String token = list.get(0);
+        list.remove(token);
+        while (!token.equals(";")){
+            if (token.equals(")")){
+                String right = s1.pop();
+                String oper = s1.pop();
+                String left = s1.pop();
+                s1.push(left+right+oper);
+            }
+            else{
+                if (!token.equals("(")){
+                    s1.push(token);
+                }
+            }
+            token = list.get(0);
+            list.remove(token);
+        }
+
+        String result = s1.peek();
+        System.out.println(result);
+
+        /*
         //Stack<String> s1 = new Stack<String>();
         if (args.length<1){
             System.out.println("Please add inputs");
